b391c795e963f3742ad79b603c51895c20189d57
ngulati8 <131073597+ngulati8@users.noreply.github.com>
2024-04-01 17:26:26 -0400

    Tested code works for Quadrature encoder, led and speaker

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ RotaryEncoder.onRotateEvent(RotationDirection.Left, function () {
     1
     )
     if (count < 1023) {
-        count += 1
+        count += 10
     }
 })
 RotaryEncoder.onRotateEvent(RotationDirection.Right, function () {
@@ -17,10 +17,11 @@ RotaryEncoder.onRotateEvent(RotationDirection.Right, function () {
     1
     )
     if (count > -1023) {
-        count += -1
+        count += -10
     }
 })
 let count = 0
+let negcount = 0
 OLED12864_I2C.init(60)
 OLED12864_I2C.on()
 OLED12864_I2C.showString(
@@ -30,6 +31,7 @@ OLED12864_I2C.showString(
 1
 )
 led.enable(false)
+servos.P1.setAngle(90)
 RotaryEncoder.init(DigitalPin.P5, DigitalPin.P6, DigitalPin.P7)
 OLED12864_I2C.clear()
 count = 0
@@ -38,14 +40,18 @@ pins.analogWritePin(AnalogPin.P2, 0)
 pins.analogWritePin(AnalogPin.P3, 0)
 basic.forever(function () {
     if (count > 0) {
+        pins.analogWritePin(AnalogPin.P3, 0)
         pins.analogWritePin(AnalogPin.P2, count)
         basic.pause(100)
     }
     if (count < 0) {
-        pins.analogWritePin(AnalogPin.P3, count)
+        pins.analogWritePin(AnalogPin.P2, 0)
+        pins.analogWritePin(AnalogPin.P3, 0 - count)
         basic.pause(100)
     }
     if (count == 1023 || count == -1023) {
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Prelude), music.PlaybackMode.InBackground)
+        basic.pause(500)
+        count = 0
     }
 })
